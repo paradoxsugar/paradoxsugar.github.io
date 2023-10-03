6679b7a79d853c84aaa642abf0008fa20ebba058
paradoxsugar <liujiachen3055@outlook.com>
2023-10-03 12:19:48 +0800

    Site updated: 2023-10-03 12:19:48

--- js/project.js
+++ js/project.js
@@ -1,23 +1,21 @@
-
-todolist();
+// 清单函数
 function todolist() {
-    fetch('https://selfmemos.zeabur.app/api/v1/memo?creatorId=SilverBullet&tag=清单').then(res => res.json()).then(data => {
+    fetch('https://selfmemos.zeabur.app/api/v1/memo?creatorId=SilverBullet&tag=清单').then(res => res.json()).then(data => { // 注意替换链接和ID
         // 获取并处理数据
+        data = data.data
+        let box = document.getElementById('todolist')
         data.forEach(item => {
             // 处理数据
             let content = item.content,
-                title = content.match(/\[(.*?)\]/g)[0].replace(/\[(.*?)\]/, '$1');
+                title = content.match(/\[(.*?)\]/g)[0].replace(/\[(.*?)\]/,'$1');
             // 去掉多余内容，替换清单内容
             content = content.replace(/#.*\s/g, '').replace(/(-\s\[\s\]\s)(.*)/g, `<li><i style="margin-right: 5px;" class="fa-regular fa-circle"></i>$2</li>`).replace(/(-\s\[x\]\s)(.*)/g, `<li class="achieve"><i style="margin-right: 5px;" class="fa-regular fa-circle-check"></i>$2</li>`);
             // 渲染数据
             let div = document.createElement('div');
             div.className = 'list_item';
             div.innerHTML = `<h3>${title}</h3><ul>${content}</ul>`;
-            document.getElementById('todolist').appendChild(div);
+            box.appendChild(div);
         });
         waterfall('#todolist');
     }).catch()
 }
-
-// 清单函数
-
