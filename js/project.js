b3e0f88c593e5a4f2aea65022c1f4b830eb5d465
paradoxsugar <liujiachen3055@outlook.com>
2023-10-25 23:35:39 +0800

    Site updated: 2023-10-25 23:35:39

--- js/project.js
+++ js/project.js
@@ -1,10 +1,1 @@
-function whenDOMReady() {
-	window.addEventListener('load', function() {
-		if (location.pathname == '/project/') waterfall('#todolist');
-	});
-}
-whenDOMReady()
-document.addEventListener("pjax:complete", whenDOMReady)
-
-// 清单函数适配pjax
-
+function whenDOMReady(){window.addEventListener("load",(function(){"/project/"==location.pathname&&waterfall("#todolist")}))}whenDOMReady(),document.addEventListener("pjax:complete",whenDOMReady);
