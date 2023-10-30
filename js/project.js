5c74424966c3c98a9029082f88b8a5cb513b94ff
paradoxsugar <liujiachen3055@outlook.com>
2023-10-30 22:34:21 +0800

    Site updated: 2023-10-30 22:34:20

--- js/project.js
+++ js/project.js
@@ -1,1 +1,10 @@
-function whenDOMReady(){window.addEventListener("load",(function(){"/project/"==location.pathname&&waterfall("#todolist")}))}whenDOMReady(),document.addEventListener("pjax:complete",whenDOMReady);
+function whenDOMReady() {
+	window.addEventListener('load', function() {
+		if (location.pathname == '/project/') waterfall('#todolist');
+	});
+}
+whenDOMReady()
+document.addEventListener("pjax:complete", whenDOMReady)
+
+// 清单函数适配pjax
+
--- js/random.js
+++ js/random.js
@@ -1,1 +1,13 @@
-function random(){fetch("/sitemap.xml").then((t=>t.text())).then((t=>(new window.DOMParser).parseFromString(t,"text/xml"))).then((t=>{let e,n,l=t.querySelectorAll("url loc");do{e=l[Math.floor(Math.random()*l.length)].innerHTML,n=e.split("/")[3]||""}while(""==n||"tags"==n);location.href=e}))}
+function random() {
+    fetch('/sitemap.xml').then(res => res.text()).then(str => (new window.DOMParser()).parseFromString(str, "text/xml")).then(data => {
+        let ls = data.querySelectorAll('url loc');
+        let locationHref,locSplit;
+        do {
+            locationHref = ls[Math.floor(Math.random() * ls.length)].innerHTML
+            locSplit = locationHref.split('/')[3] || ''
+        } while (locSplit == '' || locSplit == 'tags');
+        //若所有文章都如 https://…….com/posts/2022/07/…… 格式，主域名后字符是 posts，则循环条件改为：
+        //while (locSplit !== 'posts');
+        location.href = locationHref
+    })
+}
